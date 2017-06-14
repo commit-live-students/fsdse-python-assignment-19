7715304e995434bbe5e224496748476a57b2e1d7
RupaliHangekar <RupaliHangekar@users.noreply.github.com>
2017-06-14 18:21:04 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,7 @@
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    if key1 in dic1:
+        return True
+    else:
+        return False
+
+print (solution({1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60},2))        
