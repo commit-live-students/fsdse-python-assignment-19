7c24693d87b8d29b042be596377741f0008b45da
birajparikh16 <birajparikh16@gmail.com>
2017-06-18 17:52:32 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    count = 0
+    for key in dic1.keys():
+        if key == key1:
+            count += 1
+        if count == 0:
+            return False
+        else:
+            return True
