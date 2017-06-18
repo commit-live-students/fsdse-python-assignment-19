7dfa9808932ef012be9de4ab03eb3950f4eaf556
nixen420 <nikhil.singh4008@gmail.com>
2017-06-18 03:59:47 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
+dic1={1:'nikhi',2:'bijit'}
+key1=2
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    if dic1.has_key(key1):
+        return True
+    else:
+        return False
+solution(dic1,key1)
