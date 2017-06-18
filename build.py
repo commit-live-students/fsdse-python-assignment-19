d3f7d2d3fa211927070980d15cf3ccdef26850b6
birajparikh16 <birajparikh16@gmail.com>
2017-06-18 17:54:27 +0000

    Done

--- build.py
+++ build.py
@@ -2,8 +2,6 @@ def solution(dic1, key1):
     count = 0
     for key in dic1.keys():
         if key == key1:
-            count += 1
-        if count == 0:
-            return False
-        else:
             return True
+        else:
+            return False
