8d31bd26ba9dcd18aaf9591a90eafd9a359e46be
Rohan2015 <rohandamodar@outlook.com>
2017-06-19 05:39:08 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    for key in dic1.keys():
+        if key == key1:
+            #print True
+            return True
+        else:
+            #print False
+            return False
+
+#dic1 = {10:1, 20:2, 30:3}
+#key1 = 40
+#solution(dic1, key1)
