4a9e19e4b697bebd6ab7f96588af8c91c1c3c21f
gowthamdongari <gowthamdnr@gmail.com>
2017-06-19 15:12:55 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,11 @@
 def solution(dic1, key1):
     '''Enter Code Here'''
+    sol = 0
+    for keys_check in dic1.keys():
+        if key1 == keys_check:
+            sol +=1
+
+    if sol == 0:
+        return False
+    else:
+        return True
