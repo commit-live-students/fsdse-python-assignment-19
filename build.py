aa44c5de626d8d1d3241a5b7cabcf41921dfb76b
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-20 14:07:30 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,16 @@
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    key = key1
+    my_dict = dic1
+    for k, v in my_dict.items() :
+         if key == k :
+            return True
+         else :
+            return False
+
+
+
+dict_available = {"arg1":3,"arg2":4}
+
+print solution(dict_available ,"arg3")
+
+print solution(dict_available ,"arg1")
