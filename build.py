b29b2f2669c5be4698aaa72f0f99cd9b574963b3
ramkrishan-sahu <ram04190@gmail.com>
2017-06-21 13:00:58 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,20 @@
-def solution(dic1, key1):
-    '''Enter Code Here'''
+# Given key already exists in a dictionary
+####Doing
+
+# Given key already exists in a dictionary
+
+x = {"a": 10, "b": 20, "c": 30, "d": 40}
+
+def solution(a, b):
+    c = 0
+    for i in a.keys():
+        if (i == b):
+            print ("Key: ", b, " is present")
+            return True
+        else:
+            continue
+#     if (c == 0 ):
+#         print ("Key: ", b, " is not present")
+    return False
+val = "c"
+solution(x, val)
