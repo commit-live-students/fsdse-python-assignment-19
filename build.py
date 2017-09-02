7a3c20b836ff407aec8bb5a349162e96c10679dc
vinod-boga <vinodboga17@gmail.com>
2017-09-02 07:25:56 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
-def solution(dic1, key1):
-    '''Enter Code Here'''
+def solution(d1,Key1):
+    for key in d1:
+        if key == Key1:
+            return True
+    else:
+        return False
+
+print solution({'abc':20,'efg':10},'efg')
